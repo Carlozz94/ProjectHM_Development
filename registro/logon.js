7b8d5380eb9da7ca83fec109044bf04102f48558
geradevtab <everythingissofast@gmail.com>
2023-03-31 10:14:19 -0600

    archivos de login, con sus funciones en js

--- registro/logon.js
+++ registro/logon.js
@@ -5,22 +5,3 @@ function redireccionIniciarSesion() {
 function redireccionRegistro() {
   location.href = "./registro.html";
 }
-
-const formulario = document.querySelector("#registroUsuarios");
-
-formulario.addEventListener("submit", validarFormulario);
-
-function validarFormulario(e) {
-  e.preventDefault();
-  const nombre = document.querySelector("#nombre").value;
-  const apellido = document.querySelector("#apellido").value;
-  const telefono = document.querySelector("#telefono").value;
-  const email = document.querySelector("#email").value;
-
-  const nombreUsuario = document.getElementById("nombreUsuario");
-  nombreUsuario.textContent = `${nombre}`;
-}
-
-function redireccionPerfil() {
-  location.href = "./perfil.html";
-}
